fa1bcfe22c0473670b1eee3e4e9601bf0ce4dabf
Oscar Robertson <86749444+itdepends-music@users.noreply.github.com>
2023-10-23 00:16:20 +1300

    Complete exercise 2.13: phonebook step 8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const SearchFilterInput = ({ filterWith, handleFilterWithChange }) => {
   return (
@@ -57,8 +57,8 @@ const App = () => {
   const [filterWith, setFilterWith] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then((response) => {
-      setPersons(response.data)
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons)
     })
   }, [])
 
@@ -90,11 +90,9 @@ const App = () => {
     setNewName('')
     setNewNumber('')
 
-    axios
-      .post('http://localhost:3001/persons', personObject)
-      .then((response) => {
-        setPersons(persons.concat(response.data))
-      })
+    personService.create(personObject).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson))
+    })
   }
 
   const personsFiltered = persons.filter((person) =>
