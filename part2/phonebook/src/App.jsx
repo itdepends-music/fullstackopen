4d01fe8a8eac257f9f9af2ab08a4b1d71cdd9b2b
Oscar Robertson <86749444+itdepends-music@users.noreply.github.com>
2023-10-22 13:02:23 +1300

    Complete exercise 2.3: Course information step 8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Arto Hellas', number: '040-1234567', id: 1 },
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterWith, setFilterWith] = useState('')
+  const [nextId, setNextId] = useState(persons.length + 1)
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -13,6 +15,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterWithChange = (event) => {
+    setFilterWith(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -23,13 +29,25 @@ const App = () => {
       return
     }
 
-    setPersons(persons.concat({ name: newName, number: newNumber }))
+    setPersons(persons.concat({ name: newName, number: newNumber, id: nextId }))
+    setNextId(nextId + 1)
     setNewName('')
+    setNewNumber('')
   }
+
+  const personsFiltered = persons.filter((person) =>
+    person.name.toLowerCase().includes(filterWith.toLowerCase()),
+  )
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with{' '}
+        <input value={filterWith} onChange={handleFilterWithChange} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -44,8 +62,10 @@ const App = () => {
 
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
-          <div key={person.name}>{person.name} {person.number}</div>
+        {personsFiltered.map((person) => (
+          <div key={person.id}>
+            {person.name} {person.number}
+          </div>
         ))}
       </div>
     </div>
